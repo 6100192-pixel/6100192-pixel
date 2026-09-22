ac979e5eb530e565d9dea98b9511df406fbd9298
6100192-pixel <6100192@spsd.sk.ca>
2026-09-22 14:08:24 -0600

    Day 2 (9/22/26)
    Oh yeah supa cool

--- Interactive Scene/sketch.js
+++ Interactive Scene/sketch.js
@@ -2,26 +2,32 @@
 // Jack Breckon
 // Sept 21, 2026
 // Making a scene with an artist mark, A protagonist, Interactivity, and a Scene
-
-
+let mood = "happy"
+let currentBack = 0
+let bg = 0;
 async function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   /// Creation of Protagonist
-  background(0);
+  noStroke();
+  background(bg);
   fill(150,250,150);
   rect(mouseX-50, 200, 25, 100);
   circle(mouseX, 200, 100);
   rect(mouseX+25, 200, 25, 100);
-  noStroke();
+  
+  /// Making mouth
+  stroke(0)
+  arc(mouseX,220, 30,30,);
+  noStroke()
   /// Creation of the Scene
-  fill(200,200,200);
+  fill(255, 218, 66);
   circle(windowWidth/2,windowHeight*2, windowHeight * 3.4);
-  fill(225,225,225);
+  fill(255, 226, 108);
   circle(windowWidth/2*1.3,windowHeight*0.6, 200);
-  fill(100,100,100)
+  fill(255, 205, 0);
   circle(windowWidth/2*0.8,windowHeight*0.8, 300);
 
 
@@ -32,8 +38,28 @@ function draw() {
   text("Jack Breckon",50,windowHeight-20);
 }
 
-function keyPressed() { /// for Interactivity
-  if (key === 'space') {
-     
+function mousePressed() {
+  if (mouseButton.center === true){
+    if(bg === 0){
+      currentBack = 1;
+      bg = 125;
+    }
+    else if(bg === 125){
+      bg = 200;
+      currentBack = 2;
+    }
+    else if(bg === 200){
+      currentBack = 3;
+      bg = 255;
+    }
+    else if(bg === 255){
+      currentBack = 4;
+      bg = 0;
+    }
+  }
+  if (mouseButton.left === true){
+    mood = "sad";
+  } else if(mood === "sad"){
+    mood = "happy";
   }
 }
